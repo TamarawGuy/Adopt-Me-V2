2cf97ff9cee1d9d303d8ce7f34397648864e9fba
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-11-04 17:16:39 +0200

    Feat: Adopted pet context for displaying the adopted pet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// Components
 import SearchParams from "./components/SearchParams";
 import Details from "./components/Details";
+
+// Context
+import AdoptedPetContext from "./context/AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,16 +20,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
