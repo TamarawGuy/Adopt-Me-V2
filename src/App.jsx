55ca5e4e93690f6828401de271c3506fd210dc11
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-11-04 12:14:37 +0200

    Details page for single Pet, add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SearchParams from "./components/SearchParams";
+import Details from "./components/Details";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
